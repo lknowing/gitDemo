118005f7eb01494738f76c9790dcc85a834d711c
lknowing <1337022891@qq.com>
2022-06-14 22:11:54 +0800

    first commit

---
+++ src/main/java/com/study/test.java
@@ -0,0 +1,17 @@
+package com.study;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * title:
+ *
+ * @Author xu
+ * @Date 2022/06/14 22:09
+ * @FileName: test
+ */
+public class test {
+    @Test
+    public void test001() {
+        System.out.println("-------test-------");
+    }
+}
